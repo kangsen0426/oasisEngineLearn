223cb52ed7e2290d7a61e76c0d8d30d2ad4b112c
“KANG” <2983973848@qq.com>
2021-12-09 20:15:32 +0800

    init

---
+++ src/components/cube/cube.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <canvas id="canvas" style="width: 100vw; height: 100vh" />
+  </div>
+</template>
+
+<script>
+import {
+  WebGLEngine, //webGL 引擎
+  Camera,     //相机
+  MeshRenderer, //网格渲染器，使用网格对象（这个例子中就是立方体）作为几何体轮廓的数据源
+  PrimitiveMesh, //基础几何体，提供了创建立方体、球体等网格对象的便捷方法。
+  BlinnPhongMaterial, //材质定义了如何渲染这个立方体，BlinnPhong 材质是经典的材质之一
+  DirectLight,  //直接光，是光照的一种，光照使场景更有层次感，使用光照，能建立更真实的三维场景
+  Script,     //脚本，是衔接引擎能力和游戏逻辑的纽带，可以通过它来扩展引擎的功能，也可以脚本组件提供的生命周期钩子函数中编写自己的游戏逻辑代码。
+  Vector3,
+  Vector4,
+  Color,
+} from "oasis-engine";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/cube">
+      <div class="list_item">正方体</div>
+    </router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
+
+<style lang="less">
+
+*{
+  text-decoration: none;
+}
+.list_item{
+  width:50vw;
+  height: 40px;
+  margin: 0 auto;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  font-size: 28px;
+  border-radius: 10px;
+  background: orange;
+}
+</style>
